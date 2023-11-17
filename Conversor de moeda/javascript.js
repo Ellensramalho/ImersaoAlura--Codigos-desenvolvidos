f7ea469878436385edff3e12baa29d2956b2a0f2
Ellen Ramalho <ellen_santos_bsf@hotmail.com>
2023-11-17 17:35:28 -0300

    conversor

---
+++ Conversor de moeda/javascript.js
@@ -0,0 +1,6 @@
+var valorEmEthereum = 40;
+var cotacaoDoEthereum = 9.516;
+var nome = "Ellen";
+var valorEmReal = valorEmEthereum * cotacaoDoEthereum;
+valorEmReal = valorEmReal.toFixed(2);
+alert(nome + " R$ " + valorEmReal);
