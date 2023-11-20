a321f940490ae8a906c9483ebe28c03040ee8d9d
Ellen Ramalho <ellen_santos_bsf@hotmail.com>
2023-11-19 22:11:01 -0300

    aluraflix

---
+++ Aluraflix/aluraflix.js
@@ -0,0 +1,4 @@
+var listaFilmes = ["https://m.media-amazon.com/images/S/pv-target-images/0415cbfc768ee54688283037d793a5f664a827f09188b4dec6bc8209c45598d1.jpg","https://proxy.olhardigital.com.br/wp-content/uploads/2021/07/meninas-malvadas-paramount2.jpg","https://br.web.img3.acsta.net/medias/nmedia/18/90/84/17/20115773.jpg", "https://images-na.ssl-images-amazon.com/images/S/pv-target-images/df8fe56ff0c682287655c82c2c210625c458e39f66e0a3ac9cc1ec4bfdc74ff0._RI_TTW_.jpg"]
+
+for (var i=0; i < listaFilmes.length; i++)
+  document.write('<img src = '+ listaFilmes[i] + '>');
